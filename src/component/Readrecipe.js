b8611c05a10a34c30624968693f7815ff0ecc053
Belhrak Ali <140948858+nightitachi@users.noreply.github.com>
2024-09-18 05:41:43 +0100

    new

--- src/component/Readrecipe.js
+++ src/component/Readrecipe.js
@@ -1,23 +1,44 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 function Readrecipe() {
-  const {id}=useParams()
-  const[recipe, setRecipe]=useState([])
-  const[savedRecipe , setsavedRecipe] = useState([])
-  useEffect(()=>{
-    const getRecipe =()=>{
-      axios.get('http://localhost:3001/recipe/recipe-by-id'+id)
-      .then(result=>{
-        setRecipe(result.data)
-      }).catch(err=>console.log(err))
-    }
-    const fetchSavedRecipe =()=>{
-      axios.get('http://localhost:3001/recipe/saved-recipes'+id)
-      .then(result=>{
-        setRecipe(result.data)
-      }).catch(err=>console.log(err)) }})
+  const { id } = useParams();
+  const [recipe, setRecipe] = useState({});
+  const [savedRecipe, setSavedRecipe] = useState([]);
+
+  useEffect(() => {
+    const getRecipe = () => {
+      axios
+        .get('http://localhost:3001/recipe/recipe-by-id/' + id) // Added '/' before id
+        .then(result => {
+          setRecipe(result.data);
+        })
+        .catch(err => console.log(err));
+    };
+
+    const fetchSavedRecipe = () => {
+      axios
+        .get('http://localhost:3001/recipe/saved-recipes/' + id) // Added '/' before id
+        .then(result => {
+          setSavedRecipe(result.data.savedRecipe); // Fixed to use setSavedRecipe
+        })
+        .catch(err => console.log(err));
+    };
+
+    // Call the functions to fetch data
+    getRecipe();
+    fetchSavedRecipe();
+  }, [id]);
+
+  const saveRecipe = (recipeId) => {
+    axios
+      .put('http://localhost/recipes', { id, recipeId })
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => console.log(err));
+  };
 
   return (
     <>
@@ -27,7 +48,7 @@ function Readrecipe() {
   
       <div className="p-2">
         <h2>{recipe.name}</h2>
-        <button className='btn btn-warning' onClick={()=>savedRecipe(recipe._id)}>Save</button>
+        <button className='btn btn-warning' onClick={() => saveRecipe(recipe._id)}>Save</button>
         <h3>Description</h3>
         <p>{recipe.description}</p>
         <h2>Ingredients</h2>
@@ -35,8 +56,7 @@ function Readrecipe() {
         <img src={recipe.imgurl} alt="" />
       </div>
     </>
-  )
-  
+  );
 }
 
-export default Readrecipe
+export default Readrecipe;
